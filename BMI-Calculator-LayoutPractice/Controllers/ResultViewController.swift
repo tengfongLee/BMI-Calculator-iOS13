15404a4da47d49859a636f8d7e852c24dda0f07f
tengfongLee <tel003@ucsd.edu>
2023-08-29 01:54:57 -0700

    Finished

--- BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ResultViewController: UIViewController {
 
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
     
     @IBOutlet weak var adviceLabel: UILabel!
     @IBOutlet weak var bmiLabel: UILabel!
@@ -18,7 +20,9 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        bmiLabel.text = bmiValue!
+        bmiLabel.text = bmiValue //Label.text actually required optional string
+        adviceLabel.text = advice
+        view.backgroundColor = color // in UIview controller we have a property called view, which is also the view inside the main.storyBoard
         
         // Do any additional setup after loading the view.
     }
--- BMI-Calculator-LayoutPractice/Models/CalculatorBrain.swift
+++ BMI-Calculator-LayoutPractice/Models/CalculatorBrain.swift
@@ -19,9 +19,9 @@ struct CalculatorBrain{
     
     mutating func calculateBMI(height: Float, weight: Float){
         let bmiValue = weight / pow(height, 2)
-        var underWeightColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
-        var normalWeightColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-        var overWeightColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        let underWeightColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        let normalWeightColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        let overWeightColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         if bmiValue < 18.5 {
             bmi = BMI(value: bmiValue, advice: "Eat more pies", color: underWeightColor )
         }else if bmiValue < 24.9{
@@ -29,7 +29,15 @@ struct CalculatorBrain{
         }else{
             bmi = BMI(value: bmiValue, advice: "Eat less pies", color: overWeightColor)
         }
-        
+    }
+    
+    func getAdvice()->String{
+        let advice = String(bmi?.advice ?? "No advice")
+        return advice
+    }
+    func getColor()->UIColor{
+        let color = bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return color
     }
     
     
